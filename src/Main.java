c91c7baf435ff02ea857c80235b15084019475dd
fanghui <fanghuiplus@163.com>
2021-01-17 09:53:30 +0800

    ccccc

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(getFibonacciN(n));
+    }
+
+    // 斐波那契 递归
+    private static BigInteger getFibonacciN(int n) {
+        if(n == 1 || n == 2) {
+            return BigInteger.ONE;
+        }
+        return cycle(BigInteger.ONE, BigInteger.ONE, n -3);
+    }
+    private static BigInteger cycle(BigInteger s, BigInteger e, int n) {
+        if(n == 0) {
+            return s.add(e);
+        }
+        return cycle(e, s.add(e), --n);
     }
 }
